632f9c0828cb9e4b27e9447c2b87bffcbd810490
Ambre Kervoelen <ambre.kervoelen@etu.u-paris.fr>
2026-04-23 12:15:41 +0200

    right version square code

--- Week-2/Exercises/square.py
+++ Week-2/Exercises/square.py
@@ -5,11 +5,12 @@ control.initialize(exp)
 
 fixation = stimuli.FixCross() 
 
-square = stimuli.Rectangle(size =(50, 50), colour=(0, 0, 255))
+square = stimuli.Rectangle(size =(50, 50),colour = (0, 0, 255))
 
 control.start(subject_id=1)
 
-fixation.present(clear=True, update=True)
+square.present(clear=True, update = False)
+fixation.present(clear=False, update=True)
 
 exp.clock.wait(1000)
 
