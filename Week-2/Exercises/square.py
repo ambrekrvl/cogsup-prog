2971fd49654dfe606ea072d98848a2c310824c2a
Ambre Kervoelen <ambre.kervoelen@etu.u-paris.fr>
2026-04-23 12:00:51 +0200

    succeed square

--- Week-2/Exercises/square.py
+++ Week-2/Exercises/square.py
@@ -3,6 +3,8 @@ from expyriment import design, control, stimuli
 exp=design.Experiment(name="square")
 control.initialize(exp)
 
+fixation = stimuli.FixCross() 
+
 square = stimuli.Rectangle(size =(50, 50), colour=(0, 0, 255))
 
 control.start(subject_id=1)
