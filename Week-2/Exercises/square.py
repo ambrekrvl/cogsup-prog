b83fb04a7695d4de6766f3303b5e706a024a1a87
Ambre Kervoelen <ambre.kervoelen@etu.u-paris.fr>
2026-04-23 11:54:31 +0200

    square done

---
+++ Week-2/Exercises/square.py
@@ -0,0 +1,18 @@
+from expyriment import design, control, stimuli
+
+exp=design.Experiment(name="square")
+control.initialize(exp)
+
+square = stimuli.Rectangle(size =(50, 50), colour=(0, 0, 255))
+
+control.start(subject_id=1)
+
+fixation.present(clear=True, update=True)
+
+exp.clock.wait(1000)
+
+square.present(clear=True, update = True)
+
+exp.keyboard.wait()
+
+control.end()
